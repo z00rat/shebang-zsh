ae85ca718bd38a92d0b96004e3337945000dc8f0
zoorat <78788887+z00rat@users.noreply.github.com>
2024-11-07 21:17:38 +0600

    MOD: now it execs for ~3ms not ~5ms

--- etc/zsh/banner.sh
+++ etc/zsh/banner.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
 
-f=3 b=4
-for j in f b; do
-	for i in {0..7}; do
-		printf -v $j$i %b "\e[${!j}${i}m"
-	done
-done
-d=$'\e[1m'
-t=$'\e[0m'
-v=$'\e[7m'
+# Directly assign color codes for the foreground
+f1=$'\e[31m'  # Red
+f2=$'\e[32m'  # Green
+f3=$'\e[33m'  # Yellow
+f4=$'\e[34m'  # Blue
+f5=$'\e[35m'  # Magenta
+f6=$'\e[36m'  # Cyan
+f7=$'\e[37m'  # White
+bold=$'\e[1m' # Bold
+re=$'\e[0m'   # Reset
 
-cat <<EOF
+block="███████${bold}██${re}" # Combined block format
 
- $f1███████$d██$t $f2███████$d██$t $f3███████$d██$t $f4███████$d██$t $f5███████$d██$t $f6███████$d██$t
- $f1███████$d██$t $f2███████$d██$t $f3███████$d██$t $f4███████$d██$t $f5███████$d██$t $f6███████$d██$t
- $f1███████$d██$t $f2███████$d██$t $f3███████$d██$t $f4███████$d██$t $f5███████$d██$t $f6███████$d██$t
- $ft███████$d$f7██$t $ft███████$d$f7██$t $ft███████$d$f7██$t $ft███████$d$f7██$t $ft███████$d$f7██$t $ft███████$d$f7██$t
+# Build the repeated line content with color variables
+line=" ${f1}${block} ${f2}${block} ${f3}${block} ${f4}${block} ${f5}${block} ${f6}${block}\n"
+last=" ${f7}${block} ${f7}${block} ${f7}${block} ${f7}${block} ${f7}${block} ${f7}${block}\n"
 
-EOF
+# Print the entire buffer at once
+printf "\n$line$line$line$last\n"
 
